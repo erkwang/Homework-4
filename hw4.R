deaa58a5903dfd1159f81edbf4301bceeded93cc
erkwang <erikent@yahoo.com>
2013-03-03 23:30:14 -0800

    R attempt
    Try to solve the assignment with R code on 2008.csv file

--- hw4.R
+++ hw4.R
@@ -3,15 +3,21 @@
 
 #counting in R with file connection
 #create connection to .bz2 file
-con = bzfile("~/Downloads/2008.csv.bz2", open="rt")
-#read the first one hundred lines
-lines.r = readLines(con, n = 100)
+con = file("~/Downloads/2008.csv", open="rt")
+#read the first one hundred lines as a block
+lines.r = readLines(con, n = 10000)
+#split the block into columns
+lines.r = strsplit(lines.r, split=",")
+#grab the 17th column
+airports = sapply(lines.r, '[[', 17)
 #construct vector for counting airports
-counts = structure(0, names = "LAX")
+counts = structure(integer(4), names = c("LAX", "OAK","SFO", "SMF"))
+counts[names(counts)] = table(airports)[names(counts)]
+counts[is.na(counts)] = 0
 #while loop to read rest of the file
 while (!is.null(lines.r)) {
   #count the lines containing the name in counts
-  counts[names(counts)] = sum(grepl(names(counts), lines.r))
+  counts = counts + table(airports)[names(counts)]
   #read the next 100 lines in connection
-  lines.r = lines.r = readLines(con, n = 100)
+  lines.r = lines.r = readLines(con, n = 10000)
 }
