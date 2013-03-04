ed5cc8ce4bc8159bbcf7d0cd2a1fdbb567823ecd
erkwang <erikent@yahoo.com>
2013-03-04 11:49:31 -0800

    Turn R code into a function

--- hw4.R
+++ hw4.R
@@ -2,23 +2,33 @@
 #YICHUAN WANG
 
 #counting in R with file connection
-#create connection to the file
-con = file("~/Downloads/2008.csv", open="rt")
-#construct vector for counting airports
-counts = structure(integer(4), names = c("LAX", "OAK","SFO", "SMF"))
-#while loop to read rest of the file
-while (TRUE) {
-  #read the next 100 lines in connection
-  lines.r = readLines(con, n = 1000000)
-  #check if all the file has been read
-  if (length(lines.r) == 0) break
-  #split the block into columns
-  lines.r = strsplit(lines.r, split=",")
-  #grab the 17th column
-  airports = sapply(lines.r, '[[', 17)
-  #count the lines containing the name in counts
-  tmp = table(airports)[names(counts)]
-  tmp[is.na(tmp)] = 0
-  #add it to counts
-  counts = counts + tmp
+#set the file to be read
+filepath = "~/Downloads/Years1987_1999.tar"
+
+#function to get the counts for each airports
+apcount = function(path){
+  #get the line count of the file with shell command
+  totallines = system(paste("wc -l", path), intern = TRUE)
+  totallines = as.numeric(gsub("[space]*([0-9]+) .+", "\\1", totallines))
+  #create connection to the file
+  con = file(filepath, open="rt")
+  #construct vector for counting airports
+  counts = structure(integer(4), names = c("LAX", "OAK","SFO", "SMF"))
+  #while loop to read rest of the file
+  while (TRUE) {
+    #read about one tenth of total lines in connection
+    lines.r = readLines(con, n = floor(totallines/10))
+    #check if all the file has been read
+    if (length(lines.r) == 0) break
+    #split the block into columns
+    lines.r = strsplit(lines.r, split=",")
+    #grab the 17th column
+    airports = sapply(lines.r, '[[', 17)
+    #count the lines containing the name in counts
+    tmp = table(airports)[names(counts)]
+    tmp[is.na(tmp)] = 0
+    #add it to counts
+    counts = counts + tmp
+  }
+  counts
 }
