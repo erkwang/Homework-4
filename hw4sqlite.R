77b89c9f6b2298ff788815abd21e08c6067a34f7
erkwang <erikent@yahoo.com>
2013-03-06 08:14:39 -0800

    SQLite analysis in R

---
+++ hw4sqlite.R
@@ -0,0 +1,26 @@
+#SQLite database operations
+
+#R code for combining all csv files
+
+csvls = list.files("~/Downloads/", pattern = "[0-9]+\\.csv", full.names=TRUE,
+                   recursive=TRUE)
+writecon = file("~/Downloads/allairline.csv", open = "at")
+
+sapply(csvls, function(path){
+  readcon = file(path, open="rt")
+  foo = readLines(readcon)[-1]
+  
+  close(readcon)
+})
+
+close(writecon)
+
+#the database airline.db was created in sqlite with given code and the csv file
+
+#
+
+
+
+
+
+
